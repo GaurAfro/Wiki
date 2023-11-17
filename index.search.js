f9a753c1232b4539fad98e0800f4711671c48329
dhij <davidhwang.ij@gmail.com>
2023-11-17 17:24:20 +0000

    Updated site

---
+++ index.search.js
@@ -0,0 +1,50 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "Intro Basics Discover what this Hugo theme is all about and the core concepts behind it.\n",
+    "description": "",
+    "tags": null,
+    "title": "Basics",
+    "uri": "/basics/index.html"
+  },
+  {
+    "breadcrumb": "Basics",
+    "content": "hugo new basics/first-content.md hugo new basics/second-content/_index.md",
+    "description": "",
+    "tags": null,
+    "title": "First Content",
+    "uri": "/basics/first-content/index.html"
+  },
+  {
+    "breadcrumb": "Basics",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Second Content",
+    "uri": "/basics/second-content/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Intro I made this so I can easily find and show the information I curated. This is only valueble to me and those that want to learn from me.\n",
+    "description": "",
+    "tags": null,
+    "title": "This Wiki",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
